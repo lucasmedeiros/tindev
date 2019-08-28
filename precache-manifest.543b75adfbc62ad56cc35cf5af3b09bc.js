7b1675a1b8adfa5875c7882fb45cfcad3d46efee
LukeHxH <lucasmed812@gmail.com>
2019-08-28 15:22:19 -0300

    Updates

---
+++ precache-manifest.543b75adfbc62ad56cc35cf5af3b09bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f99f011af2616e43a44ec22da23b1f8",
+    "url": "/tindev/index.html"
+  },
+  {
+    "revision": "83106fd8a488ac8f16a9",
+    "url": "/tindev/static/css/main.df272a35.chunk.css"
+  },
+  {
+    "revision": "e4bba41f35d53857904b",
+    "url": "/tindev/static/js/2.b1e5bcc6.chunk.js"
+  },
+  {
+    "revision": "83106fd8a488ac8f16a9",
+    "url": "/tindev/static/js/main.8f828997.chunk.js"
+  },
+  {
+    "revision": "a326cb66c3d055e27fb1",
+    "url": "/tindev/static/js/runtime~main.9d92bef2.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/tindev/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/tindev/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/tindev/static/media/logo.6a01ceef.svg"
+  }
+]);
